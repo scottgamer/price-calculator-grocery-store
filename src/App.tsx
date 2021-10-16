28fedde6faa1a9cd962b55a2482868a9fab3928b
rmunoz <richosojason@msn.com>
2021-10-16 14:36:56 -0500

    added basic ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import {
+  Button,
+  Container,
+  List,
+  ListItem,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 
 function App() {
+  const [quantity, setQuantity] = useState<number>(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Typography variant="h2">Grocery Store</Typography>
+      <Typography variant="h4">Available Products</Typography>
+
+      <List>
+        <ListItem>
+          <label>
+            Milk{" "}
+            <Button
+              variant="outlined"
+              size="small"
+              onClick={() => {
+                if (quantity < 1) {
+                  return;
+                }
+                setQuantity(quantity - 1);
+              }}
+            >
+              -
+            </Button>
+            <input type="number" name="" value={quantity} />
+            <Button
+              variant="outlined"
+              size="small"
+              onClick={() => setQuantity(quantity + 1)}
+            >
+              +
+            </Button>
+          </label>
+        </ListItem>
+        <ListItem>Apple</ListItem>
+        <ListItem>Banana</ListItem>
+      </List>
+
+      <Typography variant="h4" bottom={"bottom"}>
+        Summary
+      </Typography>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Item</TableCell>
+              <TableCell>Quantity</TableCell>
+              <TableCell>Price</TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            <TableRow>
+              <TableCell>Milk</TableCell>
+              <TableCell>2</TableCell>
+              <TableCell>$5.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <Typography> Total:</Typography>
+      <Typography>You saved:</Typography>
+    </Container>
   );
 }
 
