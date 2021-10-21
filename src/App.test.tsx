7dfddbd19edddcbf544d2d8dc1ecaf0c2244cfca
rmunoz <richosojason@msn.com>
2021-10-21 12:26:54 -0500

    added test for listing items

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+  test("renders the App component", () => {
+    render(<App />);
+    screen.getByRole("heading", { name: /Grocery Store/i });
+  });
+
+  test("lists initial products and values", () => {
+    render(<App />);
+    const listItems = screen.getAllByRole("listitem");
+    expect(listItems).toHaveLength(4);
+
+    const milkLabel = screen.getByLabelText(/Milk/i);
+    const breadLabel = screen.getByLabelText(/Bread/i);
+    const bananaLabel = screen.getByLabelText(/Banana/i);
+    const appleLabel = screen.getByLabelText(/Apple/i);
+
+    expect(milkLabel).toHaveValue(0);
+    expect(breadLabel).toHaveValue(0);
+    expect(bananaLabel).toHaveValue(0);
+    expect(appleLabel).toHaveValue(0);
+  });
 });
